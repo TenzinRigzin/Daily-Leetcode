73c0d16883820c108a5011817aaaa2ca07182443
Tenzin Rigzin <trigzin2005@gmail.com>
2026-05-20 23:19:52 +0530

    Add moveZeroes function to handle zeroes in array
    Implement function to move all zeroes to the end of the array while maintaining order.

---
+++ day003.py
@@ -0,0 +1,43 @@
+"""
+Move Zeroes
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+
+"""
+
+class Solution:
+    def moveZeroes(self, nums):
+        j = 0
+
+        # Move non-zero elements forward
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+
+        # Fill remaining positions with 0
+        while j < len(nums):
+            nums[j] = 0
+            j += 1
