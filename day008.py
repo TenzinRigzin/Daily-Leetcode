6d3c942c9e56d2e4423beb481f9f0af11655e908
Tenzin Rigzin <trigzin2005@gmail.com>
2026-06-02 22:09:02 +0530

    Add subarray sum function in day008.py
    Implement a function to count subarrays with sum equal to k.

---
+++ day008.py
@@ -0,0 +1,42 @@
+"""
+Sub Array sum equals K
+
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+
+"""
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        count = 0
+        prefix_sum = 0
+        
+      
+        prefix_count = {0: 1}
+        
+        for num in nums:
+            prefix_sum += num
+            
+       
+            if prefix_sum - k in prefix_count:
+                count += prefix_count[prefix_sum - k]
+            
+
+            prefix_count[prefix_sum] = prefix_count.get(prefix_sum, 0) + 1
+        
+        return count
